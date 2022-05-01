43fdeb4b05801e5b2aa431d7e9a9a01d7f5f6191
leaa2019 <leaa.asw2019@gmail.com>
2022-05-01 15:54:05 +0200

    start on LL

--- data_structures/LinkedList.py
+++ data_structures/LinkedList.py
@@ -1,1 +1,15 @@
+"""https://realpython.com/linked-lists-python/"""
+
 # class
+class LinkedList(object):
+    # ctor
+    def __init__(self, head):
+        pass
+
+class Node(object):
+    # ctor
+    def __init__(self, key):
+        pass
+
+    # inst methods
+    # def del
